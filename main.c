69b5e76e47a407e1812878a3f4b8d09f2bae7b59
Anders <anders.s.r@icloud.com>
2018-10-11 18:50:40 +0200

    Added some ints...

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+
+	main.c -- Template main()
+	
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main( int argc, char** argv ) {
+
+	int t = 2;
+	int x = 6;
+	int res;
+
+	res = t * x;
+
+	return 0;
+
+}
